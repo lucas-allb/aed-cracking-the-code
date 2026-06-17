86c6ac3369a3e6da9a6729f6a444d2c27e0ce96d
lucas-allb <albuquerque.trabalho5625@gmail.com>
2026-06-17 18:34:15 -0300

    questao 1

--- challenges/group_anagrams.py
+++ challenges/group_anagrams.py
@@ -1,2 +1,9 @@
 def group_anagrams(words: list[str]) -> list[list[str]]:
-    raise NotImplementedError
+    anagrama = {}
+    for word in words:
+        chave = "".join(sorted(word))
+        if chave not in anagrama:
+            anagrama[chave] = []
+        
+        anagrama[chave].append(word)
+    return list(anagrama.values())
