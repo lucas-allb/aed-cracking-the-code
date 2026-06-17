2ef44eea27df4ab71e4cb57469b0518491636524
lucas-allb <albuquerque.trabalho5625@gmail.com>
2026-06-17 19:10:58 -0300

    questao-03

--- challenges/lowest_common_ancestor.py
+++ challenges/lowest_common_ancestor.py
@@ -6,4 +6,14 @@ def lowest_common_ancestor(
     value1: int,
     value2: int,
 ) -> int:
-    raise NotImplementedError
+    no_atual = root
+
+    while no_atual is not None:
+        if value1 < no_atual.value and value2 < no_atual.value:
+            no_atual = no_atual.left
+        elif value1 > no_atual.value and value2 > no_atual.value:
+            no_atual = no_atual.riht
+        else:
+            return no_atual.value
+        
+    return -1 
