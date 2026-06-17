cb42b3a8b1dd79a15c16df2fa3000997a5004355
lucas-allb <albuquerque.trabalho5625@gmail.com>
2026-06-17 19:23:49 -0300

    questao-04

--- challenges/tree_height.py
+++ challenges/tree_height.py
@@ -2,4 +2,13 @@ from data_structures.node import Node
 
 
 def tree_height(root: Node | None) -> int:
-    raise NotImplementedError
+    if root is None:
+        return -1
+    
+    if root.left is None and root.right is None:
+        return 0
+    
+    left_heigth = tree_height(root.left)
+    right_heigth = tree_height(root.right)
+
+    return 1 + max(left_heigth, right_heigth)
